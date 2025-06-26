e10caed8a5878b8e24ee22cca79f510e2dc4d927
Julia <157003949+Gammavarius@users.noreply.github.com>
2025-06-26 13:42:20 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*Свайпер*/ 
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
@@ -17,5 +18,35 @@ const swiper = new Swiper('.swiper', {
   initialSlide: 1,
 })
 
+/*Ссылка по якорю*/
+const anchors = document.querySelectorAll('a.to-order')
+
+for(let anchor of anchors) {
+  anchor.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    const blockID = anchor.getAttribute('href')
+
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+/*Модальное окно*/
 const modalWindow = document.querySelector('.feedback__modal');
-const connectButton = document.querySelector('.connect-us')
+const popup = document.querySelector('.feedback__section');
+const openPopupButton = document.querySelector('.connect-us');
+const closePopupButton = popup.querySelector('.feedback-exit-button');
+
+openPopupButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  modalWindow.classList.add('active')
+  popup.classList.add('active')
+})
+
+closePopupButton.addEventListener('click', function() {
+  modalWindow.classList.remove('active')
+  popup.classList.remove('active')
+})
